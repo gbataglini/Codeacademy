c64907b0da804d25fca36d4b943753737aa66917
Giovanna Bataglini <118122049+gbataglini@users.noreply.github.com>
2022-12-30 11:48:43 +0000

    Add files via upload

---
+++ whaleTalk.js
@@ -0,0 +1,20 @@
+const input = 'Elephant in the room'; 
+const vowels = ['a','e','i','o','u'];
+let resultArray = [];
+for (let i = 0; i < input.length; i++) {
+  if (input[i] === 'e') {
+    resultArray.push(input[i]);
+  }
+    if (input[i] === 'u'){
+      resultArray.push(input[i]);
+    }
+  for (let j = 0; j < vowels.length; j++) {
+        if (input[i] === vowels[j]) {
+          //console.log(input[i])
+          resultArray.push(input[i]);
+        }
+}
+}
+//console.log(resultArray)
+const resultString = resultArray.join(' ').toUpperCase()
+console.log(resultString);
